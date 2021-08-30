88274685e2415cbd3f0bd68aedabb4c6b085f25e
boa <boa.matule@gmail.com>
2021-08-30 17:03:08 +0200

    New files for spacing, typograpthy and restaurant-info-card.

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,40 +1,60 @@
 import React from "react";
-import styled from "styled-components/native";
-import { Text } from "react-native";
-import { Card } from "react-native-paper";
-
-const RestaurantCard = styled(Card)`
-  background-color: ${(props) => props.theme.colors.bg.primary};
-`;
-
-const RestaurantCardCover = styled(Card.Cover)`
-  color: ${(props) => props.theme.colors.bg.primary};
-  padding: ${(props) => props.theme.space[3]};
-`;
-
-const Title = styled(Text)`
-  font-family: ${(props) => props.theme.fonts.body};
-  color: ${(props) => props.theme.colors.ui.primary};
-  padding: ${(props) => props.theme.space[3]};
-`;
+import { SvgXml } from "react-native-svg";
+import star from "../../../../assets/star";
+import open from "../../../../assets/open";
+import { Spacer } from "../../../components/searchBar/spacer/spacer.component";
+import { Text } from "../../../components/typography/text.component";
+import {
+  RestaurantCard,
+  RestaurantCardCover,
+  Info,
+  Section,
+  SectionEnd,
+  Rating,
+  Icon,
+  Address,
+} from "./restaurant-info-card.styles";
 
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
     name = "Raw Mat",
-    icon,
+    icon = "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
     photos = [
       "https://images.squarespace-cdn.com/content/v1/5be122d8f8370ab91d93d1c3/1558522665811-PBBCY36VKWGO9C5NFIK6/DSC_0866.jpg?format=2500w",
     ],
     address = "Raw 100 Ndota Street",
     isOpenNow = true,
-    rating = 4,
+    rating = 5,
     isClosedTemporarily,
   } = restaurant;
+
+  const ratingArray = Array.from(new Array(Math.floor(rating)));
 
   return (
     <RestaurantCard elevation={5}>
       <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
-      <Title>{name}</Title>
+      <Info>
+        <Text variant="label">{name}</Text>
+        <Section>
+          <Rating>
+            {ratingArray.map(() => (
+              <SvgXml xml={star} width={20} height={20} />
+            ))}
+          </Rating>
+          <SectionEnd>
+            {isClosedTemporarily && (
+              <Text variant="error">CLOSED TEMPORARILY</Text>
+            )}
+            <Spacer position="left" size="large">
+              {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
+            </Spacer>
+            <Spacer position="left" size="large">
+              <Icon source={{ uri: icon }} />
+            </Spacer>
+          </SectionEnd>
+        </Section>
+        <Address>{address}</Address>
+      </Info>
     </RestaurantCard>
   );
 };
--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,20 +1,12 @@
 import React from "react";
 import { Searchbar } from "react-native-paper";
-import { StatusBar, StyleSheet, SafeAreaView, Text, View } from "react-native";
-// import {
-//   RestaurantsScreenContainer,
-//   RestaurantsScreenSearchBar,
-//   RestaurantsScreenList,
-// } from "./RestaurantsScreenStyles";
+import { StatusBar, SafeAreaView } from "react-native";
 import styled from "styled-components/native";
-
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
 
 const SafeArea = styled(SafeAreaView)`
   flex: 1;
   margin-top: ${StatusBar.currentHeight}px;
-  /* margin-top: ${StatusBar.currentHeight &&
-  `margin-top ${StatusBar.currentHeight}px`}; */
 `;
 
 const SearchContainer = styled.View`
