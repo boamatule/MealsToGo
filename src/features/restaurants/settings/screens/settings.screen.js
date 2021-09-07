4c5e9fef8a5fb118f347cdf7e27d59af60c228b5
boa <boa.matule@gmail.com>
2021-09-08 01:38:19 +0200

    Builds up the Navigation for scale; Creates a details restaurant screen to display the restaurants info.

--- src/features/restaurants/settings/screens/settings.screen.js
+++ src/features/restaurants/settings/screens/settings.screen.js
@@ -1,9 +1,9 @@
-// import React from "react";
-// import { Text } from "react-native";
-// import { SafeArea } from "../../../components/utility/safe-area.component";
+import React from "react";
+import { Text } from "react-native";
+import { SafeArea } from "../../../components/utility/safe-area.component";
 
-// export const Settings = () => (
-//   <SafeArea>
-//     <Text>Settings</Text>
-//   </SafeArea>
-// );
+export const Settings = () => (
+  <SafeArea>
+    <Text>Settings</Text>
+  </SafeArea>
+);
