cef3364a0b7bad28186642779b1d83542717e58e
boa <boa.matule@gmail.com>
2021-09-11 17:14:11 +0200

    Setups the SettingsScreen Stack. WIP

--- src/features/settings/screens/settings.screen.js
+++ src/features/settings/screens/settings.screen.js
@@ -1,9 +1,31 @@
-import React from "react";
-import { Text } from "react-native";
-import { SafeArea } from "../../../components/utility/safe-area.component";
+import React, { useContext } from "react";
+import { List } from "react-native-paper";
 
-export const SettingScreen = () => (
-  <SafeArea>
-    <Text>Settings</Text>
-  </SafeArea>
-);
+import { SafeArea } from "../../../components/utility/safe-area.component";
+import { AuthenticationContext } from "../../../services/authentication/authentication.context";
+
+export const SettingsScreen = ({ navigation }) => {
+  const { onLogout } = useContext(AuthenticationContext);
+  return (
+    <SafeArea>
+      <List.Section>
+        <List.Item
+          // eslint-disable-next-line react-native/no-inline-styles
+          style={{ padding: 16 }}
+          title="Favourites"
+          description="View your favourites"
+          left={(props) => <List.Icon {...props} color="black" icon="heart" />}
+          onPress={() => navigation.navigate("Favourites")}
+        />
+        <List.Item
+          // eslint-disable-next-line react-native/no-inline-styles
+          style={{ padding: 16 }}
+          title="Logout"
+          description="View your favourites"
+          left={(props) => <List.Icon {...props} color="black" icon="door" />}
+          onPress={onLogout}
+        />
+      </List.Section>
+    </SafeArea>
+  );
+};
