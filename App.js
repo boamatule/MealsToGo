1288deb5ebb72e95b5321044e8726c8e078a9f55
boa <boa.matule@gmail.com>
2021-09-14 23:28:51 +0200

    Loads Google fonts

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useState } from "react";
+import AppLoading from "expo-app-loading";
 import { ThemeProvider } from "styled-components/native";
 
-import * as firebase from "firebase";
+import * as Font from "expo-font";
+// import {
+//   useFonts as useOswald,
+//   Oswald_400Regular,
+// } from "@expo-google-fonts/oswald";
+// import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 
-import {
-  useFonts as useOswald,
-  Oswald_400Regular,
-} from "@expo-google-fonts/oswald";
-import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
+import * as firebase from "firebase";
 
 import { theme } from "./src/infrastucture/theme";
 import { Navigation } from "./src/infrastucture/theme/navigation";
@@ -28,27 +30,41 @@ if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
 }
 
+// const [oswaldLoaded] = useOswald({
+//   Oswald_400Regular,
+// });
+
+// const [latoLoaded] = useLato({
+//   Lato_400Regular,
+// });
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    Lato_400Regular: require("./assets/fonts/Lato-Regular.ttf"),
+    Oswald_400Regular: require("./assets/fonts/Oswald-VariableFont_wght.ttf"),
+  });
+};
+
 export default function App() {
-  const [oswaldLoaded] = useOswald({
-    Oswald_400Regular,
-  });
+  const [fontsLoaded, setFontsLoaded] = useState(false);
 
-  const [latoLoaded] = useLato({
-    Lato_400Regular,
-  });
-
-  if (!oswaldLoaded || !latoLoaded) {
-    return null;
+  if (!fontsLoaded) {
+    return (
+      <>
+        <ThemeProvider theme={theme}>
+          <AuthenticationContextProvider>
+            <Navigation />
+          </AuthenticationContextProvider>
+        </ThemeProvider>
+        <ExpoStatusBar style="auto" />
+      </>
+    );
+  } else {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setFontsLoaded(true)}
+      />
+    );
   }
-
-  return (
-    <>
-      <ThemeProvider theme={theme}>
-        <AuthenticationContextProvider>
-          <Navigation />
-        </AuthenticationContextProvider>
-      </ThemeProvider>
-      <ExpoStatusBar style="auto" />
-    </>
-  );
 }
--- src/infrastucture/theme/fonts.js
+++ src/infrastucture/theme/fonts.js
@@ -1,7 +1,7 @@
 export const fonts = {
-  body: "Oswaldo_400Regular",
+  body: "Oswald_400Regular",
   heading: "Lato_400Regular",
-  monospace: "Oswaldo_400Regular",
+  monospace: "Oswald_400Regular",
 };
 
 export const fontWeights = {
