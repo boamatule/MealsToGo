38241910dc722036cc323566cd1f28651316b04e
boa <boa.matule@gmail.com>
2021-09-18 22:42:30 +0200

    Error fixing

--- functions/place/index.js
+++ functions/place/index.js
@@ -18,30 +18,31 @@ const addGoogleImage = (restaurant) => {
   return restaurant;
 };
 
-
 module.exports.placesRequest = (request, response, client) => {
   const {location, mock} = url.parse(request.url, true).query;
   if (mock === "true") {
     const data = mocks[location];
-    if (data ) {
+    if (data) {
       data.results = data.results.map(addMockImage);
     }
 
     return response.json(data);
   }
-  client.placesNearby({
-    params: {
-      location: location,
-      radius: 1500,
-      type: "restaurants",
-      key: functions.config().google.key,
-    },
-    timeout: 1000,
-  })
+  client
+      .placesNearby({
+        params: {
+          location: location,
+          radius: 1500,
+          type: "restaurant",
+          key: functions.config().google.key,
+        },
+        timeout: 1000,
+      })
       .then((res) => {
         res.data.results = res.data.results.map(addGoogleImage);
         return response.json(res.data);
-      }).catch((e) => {
+      })
+      .catch((e) => {
         response.status(400);
         return response.send(e.response.data.error_message);
       });
